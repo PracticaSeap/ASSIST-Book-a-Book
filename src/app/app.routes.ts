78daa965b4972e3b325f1ed1c07aa5874b1ee215
Robert Gumieni <robertg6@yahoo.com>
2019-05-03 15:41:16 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
+
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/main-login/login/login.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { AddBookComponent } from './components/admin/add-book/add-book.component';
 import { ManageBooksComponent } from './components/admin/manage-books/manage-books.component';
+import {SignUpComponent} from './components/main-login/sign-up/sign-up.component'
 
 export const appRoutes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -11,5 +13,6 @@ export const appRoutes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'book-details/:id', component: BookDetailsComponent },
     { path: 'add-book', component: AddBookComponent },
-    { path: 'manage-books', component: ManageBooksComponent }
+    { path: 'manage-books', component: ManageBooksComponent },
+    {path: 'signup', component: SignUpComponent  }
   ];
