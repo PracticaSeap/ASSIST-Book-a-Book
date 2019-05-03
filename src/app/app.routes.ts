6a269fe84476433582b272e226223d31c27b6559
Krotores <39189098+Krotores@users.noreply.github.com>
2019-05-03 15:43:43 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/main-login/login/login.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { AddBookComponent } from './components/admin/add-book/add-book.component';
 import { ManageBooksComponent } from './components/admin/manage-books/manage-books.component';
+import { ForgotPasswordComponent } from './components/main-login/forgot-password/forgot-password.component'
 
 export const appRoutes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -11,5 +12,8 @@ export const appRoutes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'book-details/:id', component: BookDetailsComponent },
     { path: 'add-book', component: AddBookComponent },
-    { path: 'manage-books', component: ManageBooksComponent }
+    { path: 'manage-books', component: ManageBooksComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent}
   ];
+
+
