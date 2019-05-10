8d660a63bbc77cf04ce9088e08841de3e4967708
Krotores <39189098+Krotores@users.noreply.github.com>
2019-05-10 16:46:41 +0300

    Update sign-up.component.ts

--- src/app/components/main-login/sign-up/sign-up.component.ts
+++ src/app/components/main-login/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { LoginService } from 'src/app/services/login.service';
+import { StringFormat } from '@angular/fire/storage/interfaces';
+import { NgIf } from '@angular/common';
+import { strictEqual } from 'assert';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  fullname: string;
+  email: string;
+  password: string;
+  userCreatedSucess = 0;
+
+  alertError: boolean = false;
+  errMessage: string= "";
+
+  succValue:boolean = false;
+  constructor(public loginservice: LoginService){}
+  //constructor(public loginservice: LoginService) {}
+
+  
 
   ngOnInit() {
+
   }
 
+  createUserWithEmailAndPassword(){
+    this.loginservice.createUserWithEmailAndPassword(this.email, this.password)
+    .then(sucess=>{
+      this.userCreatedSucess = 1;
+      this.succValue = true;
+    })
+    .catch(error =>  {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+      //alert(errorCode + ' ' + errorMessage);
+      
+      if(error.message.includes("already in use") == true)
+      {
+        this.alertError = true;
+        this.errMessage = error.message;
+      }
+
+      this.userCreatedSucess = 2;
+    });
+    // console.log(this.email);
+
+  }
+
+  onKeyEmail(event: any) { // without type info
+    this.email = event.target.value;
+    this.alertError = false;
+    this.succValue = false;
+  }
+
+  onKeyPassword(event: any) { // without type info
+    this.password = event.target.value;
+    this.alertError = false;
+    this.succValue = false;
+  }
+
+  onKeyFullName(event: any) { // without type info
+    this.fullname = event.target.value;
+    this.alertError = false;
+    this.succValue = false;
+  }
 }
