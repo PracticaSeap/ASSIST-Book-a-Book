72f1162eb272ffda5049864e4d65fa5ad80857a9
Cineva9988 <balabasciuc1@gmail.com>
2019-05-10 10:44:37 +0300

    Update book.model.ts

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,18 @@
+
+
+
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    description: string;
+    isbn: string;
+    category: string;
+    tag: string;
+    image: string;
+    number_of_pages: number;
+    virtual_book: string;
+
+   
+}
+
