932aab07236c158a33abc2e7cce55a7bf4bb44a3
Cineva9988 <balabasciuc1@gmail.com>
2019-05-10 15:45:17 +0300

    Update manage-books.component.ts

--- src/app/components/admin/manage-books/manage-books.component.ts
+++ src/app/components/admin/manage-books/manage-books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ManageBooksService } from 'src/app/services/manage-books.service';
+import { browser } from 'protractor';
 
 @Component({
   selector: 'app-manage-books',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-books.component.css']
 })
 export class ManageBooksComponent implements OnInit {
-
-  constructor() { }
+  overduebooks;
+  borrowedbooks;
+  allBooks;
+  constructor(public bookManagerService: ManageBooksService) { }
 
   ngOnInit() {
+   
+    this.bookManagerService.getbooks().subscribe(books=>{
+      this.allBooks = books;
+      console.log(this.allBooks);
+    });
+    this.bookManagerService.getbooks().subscribe(books=>{
+      this.overduebooks= books;
+      console.log(this.overduebooks);
+
+    });
+    this.bookManagerService.getbooks().subscribe(books=> {
+      this.borrowedbooks = books;
+      console.log(this.borrowedbooks);
+    });
   }
 
 }
