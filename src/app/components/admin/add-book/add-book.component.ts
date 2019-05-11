83aa1a64d0d0ccf0a60cfb34c99c99c61ecc1878
denispetriceanu <denispetriceanu@gmail.com>
2019-05-11 21:37:29 +0300

    Reactive forms - corect

--- src/app/components/admin/add-book/add-book.component.ts
+++ src/app/components/admin/add-book/add-book.component.ts
@@ -3,7 +3,8 @@ import { Book } from 'src/app/models/book.model';
 import { FormControl } from '@angular/forms';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-add-book',
@@ -12,26 +13,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 // export class FormFieldOverviewExample {}
 export class AddBookComponent implements OnInit {
-  bookType = 'PhisicalBook';
+  virtual_book = 'PhisicalBook';
+  // bookType = 'PhisicalBook';
   public book: Book;
   addBook: FormGroup;
+  // form: FormGroup;
   private books;
   constructor(public db: AngularFireDatabase,
     private fb: FormBuilder) {
     this.db.list('/books').valueChanges().subscribe(books => {
       this.books = books;
-      this.book = {
-        id: this.books.length,
-        title: '',
-        author: '',
-        isbn: '',
-        description: '',
-        category: '',
-        tag: '',
-        image: '',
-        number_of_pages: 0,
-        virtual_book: '',
-      }
+        this.book = {
+          id: this.books.length,
+          title: '',
+          author: '',
+          isbn: '',
+          description: '',
+          category: '',
+          tag: '',
+          image: '',
+          number_of_pages: 0,
+          virtual_book: '',
+        }
     });
 
     this.addBook =  this.fb.group({
@@ -45,46 +48,51 @@ export class AddBookComponent implements OnInit {
       status: this.fb.control('', Validators.required),
       image: this.fb.control('', Validators.required),
       number_of_pages: this.fb.control('', Validators.required),
-    })
-    ;
+    });
+
+    // this.addBook = new FormGroup({
+    //   title: new FormControl('', Validators.required),
+    //   author: new FormControl('', Validators.required),
+    //   isbn: new FormControl('', Validators.required),
+    //   number_of_pages: new FormControl('', Validators.required),
+    //   category: new FormControl('', Validators.required),
+    //   tag: new FormControl('', Validators.required),
+    //   virtual_book: new FormControl('', Validators.required),
+    //   status: new FormControl('', Validators.required),
+    //   image: new FormControl('', Validators.required),
+    //   description: new FormControl('', Validators.required),
+    // });
 
 
   }
 
   ngOnInit() {
+
+    // onSubmit(value){
+    //   this.firebaseService.addBook(value)
+    //   .then(
+    //     res => {
+    //       this.resetFields();
+    //       this.router.navigate(['/home']);
+    //     }
+    //   )
   }
-  // addBook = new Book({
-  //   title: new FormControl(''),
-  //   author: new FormControl(''),
-  //   isbn: new FormControl(''),
-  //   description: new FormControl(''),
-  // });
-
-
-  // onSubmit(value){
-  //   this.firebaseService.addBook(value)
-  //   .then(
-  //     res => {
-  //       this.resetFields();
-  //       this.router.navigate(['/home']);
-  //     }
-  //   )
-  // }
-  onSubmit(value) {
-    console.log("form book", value)
-  }
-  // inputFile() {
-  //   var elem = <HTMLDivElement>(document.createElement("Input"));
-  //   elem.setAttribute("type", "file");
-  //   document.body.appendChild(elem);
-  // }
-
-  // onSubmit(value){
-  //   return this.db.list('test').push({
-  //     title: value.title,
-  //     author: value.author,
-  //     description: value.description,
-  //     isbn: value.isbn,
-  //   });
+onSubmit(value){
+  console.log("form book", this.addBook.value)
 }
+
+  // onSubmit(value) {
+  //   return this.db.list('test').push({
+      //     title: value.title,
+      //     author: value.author,
+      //     description: value.description,
+      //     isbn: value.isbn,
+  //   });
+
+  // }
+}
+
+
+
+
 
