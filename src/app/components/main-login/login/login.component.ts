5c03d21c64705e87bd4a5172876219f6657fd206
Krotores <39189098+Krotores@users.noreply.github.com>
2019-05-17 12:58:05 +0300

    Update login.component.ts

--- src/app/components/main-login/login/login.component.ts
+++ src/app/components/main-login/login/login.component.ts
@@ -1,4 +1,11 @@
+import { DashboardComponent } from './../../dashboard/dashboard.component';
+import { appRoutes } from './../../../app.routes';
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+fullname: string
+email: string
+password: string
+userCreatedSucess = 1;
+alertError: boolean = false;
+errMessage: string= "";
+succValue:boolean = false;
+  constructor(public loginservice: LoginService,
+    public afAuth:AngularFireAuth,
+    private router: Router
+    ) 
+  { }
 
   ngOnInit() {
   }
+  loginUser(){
 
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password)
+    .then(success => {
+
+
+     this.succValue = true
+
+      this.redirect()
+     //setTimeout(()=>{this.router.navigate(['dashboard']);}, 3000)
+     
+
+     })
+
+
+
+     
+
+     .catch(error =>  {
+      // Handle Errors here.
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       this.alertError = true;
+       // ...
+       //alert(errorCode + ' ' + errorMessage);
+     });
+
+
+  }
+  onKeyEmail(event: any) { // without type info
+    this.email = event.target.value;
+    this.alertError = false;
+    this.succValue = false;
+  }
+
+  onKeyPassword(event: any) { // without type info
+    this.password = event.target.value;
+    this.alertError = false;
+    this.succValue = false;
+  }
+
+  redirect(){
+    if (this.succValue==true){
+    setTimeout(()=>{this.router.navigate(['dashboard']);}, 3000);
+    }
+  }
 }
