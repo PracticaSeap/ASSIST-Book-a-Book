de53e0781005a051460b132ca48b00bdd7cb8735
Alexandru Cazacincu <alexandru.cazacincu@assist.ro>
2019-05-21 15:56:37 +0300

    Sanitization

--- src/app/components/main-login/sign-up/sign-up.component.ts
+++ src/app/components/main-login/sign-up/sign-up.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
 import { LoginService } from 'src/app/services/login.service';
-import { StringFormat } from '@angular/fire/storage/interfaces';
-import { NgIf } from '@angular/common';
-import { strictEqual } from 'assert';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { User } from 'src/app/models/user.model';
-
-
 
 @Component({
   selector: 'app-sign-up',
@@ -21,63 +15,48 @@ export class SignUpComponent implements OnInit {
   password: string;
   userCreatedSucess = 0;
 
-  alertError: boolean = false;
-  errMessage: string= "";
+  alertError = false;
+  errMessage = '';
 
-  succValue:boolean = false;
-  constructor(public loginservice: LoginService,
-    public db: AngularFireDatabase){}
-  //constructor(public loginservice: LoginService) {}
+  succValue = false;
+  constructor(public loginservice: LoginService,  public db: AngularFireDatabase) { }
 
-  
+  ngOnInit() { }
 
-  ngOnInit() {
-
-  }
-
-  createUserWithEmailAndPassword(){
-    if(this.email == null || this.password == null)
-    {
+  createUserWithEmailAndPassword() {
+    if (this.email == null || this.password == null) {
       this.alertError = true;
-      this.errMessage = "Date incomplete ";
-    }
-    else
-    {
+      this.errMessage = 'Date incomplete';
+    } else {
       this.loginservice.createUserWithEmailAndPassword(this.email, this.password)
-      .then(sucess=>{
-        this.userCreatedSucess = 1;
-        this.succValue = true;
-        let user: User ={
-          email: this.email,
-          fullName: this.fullname,
-          userRole: "user",
-        }
-        ;
-        this.db.list('/users').push(user);
-        setTimeout(() =>{
-           this.db.list('/users').valueChanges().subscribe(results=>{
-            console.log(results)
-          });
-        },1000)
-             
+        .then(sucess => {
+          this.userCreatedSucess = 1;
+          this.succValue = true;
+          const user: User = {
+            email: this.email,
+            fullName: this.fullname,
+            userRole: 'user',
+          }
+            ;
+          this.db.list('/users').push(user);
+          setTimeout(() => {
+            this.db.list('/users').valueChanges().subscribe(results => {
+              console.log(results);
+            });
+          }, 1000);
 
-      })
-      .catch(error =>  {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ...
-        //alert(errorCode + ' ' + errorMessage);
-        
-        if(error.message.includes("already in use") == true)
-        {
-          this.alertError = true;
-          this.errMessage = error.message;
-        }
+        })
+        .catch(error => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if (error.message.includes('already in use') === true) {
+            this.alertError = true;
+            this.errMessage = error.message;
+          }
 
-        this.userCreatedSucess = 2;
-      });
-      // console.log(this.email);
+          this.userCreatedSucess = 2;
+        });
     }
   }
 
