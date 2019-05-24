b79a361e540a0541564d945fbd6cb62f8dffdb00
Catalin <clupascu97@gmail.com>
2019-05-24 16:14:46 +0300

    show dueDate

--- src/app/components/dashboard/dashboard-book/dashboard-book.component.ts
+++ src/app/components/dashboard/dashboard-book/dashboard-book.component.ts
@@ -1,28 +1,49 @@
-import { BookDetailsComponent } from './../../book-details/book-details.component';
-import { Book } from './../../../models/book.model';
-import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { DashboardService } from 'src/app/services/dashboard.service';
-import { Key } from 'protractor';
+import { BookDetailsComponent } from "./../../book-details/book-details.component";
+import { Book } from "./../../../models/book.model";
+import { Component, OnInit, Input, AfterViewInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { DashboardService } from "src/app/services/dashboard.service";
+import { Key } from "protractor";
+import { ManageBooksService } from "src/app/services/manage-books.service";
+import { AngularFireDatabase } from "@angular/fire/database";
 
 @Component({
-  selector: 'app-dashboard-book',
-  templateUrl: './dashboard-book.component.html',
-  styleUrls: ['./dashboard-book.component.css']
+  selector: "app-dashboard-book",
+  templateUrl: "./dashboard-book.component.html",
+  styleUrls: ["./dashboard-book.component.css"]
 })
-
 export class DashboardBookComponent implements OnInit {
-
   @Input() inputBook: Book;
+  booksHistory: History[];
+  books: Book[];
+  dueDate;
 
-  constructor(private router: Router, public dashboardService: DashboardService, private route: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    public dashboardService: DashboardService,
+    private route: ActivatedRoute,
+    public bookManagerService: ManageBooksService,
+    private db: AngularFireDatabase
+  ) {}
 
   ngOnInit() {
+    this.db
+      .list("/history")
+      .valueChanges()
+      .subscribe(entries => {
+        console.log(entries);
+        const histories = entries.filter(
+          element => this.inputBook.key === element.bookKey
+        );
+        console.log("333", histories);
+        if (histories.length > 0) {
+          this.dueDate = histories[histories.length - 1].dueDate;
+          console.log(this.dueDate);
+        }
+      });
   }
 
   onSelect() {
-    this.router.navigate(['/book-details/', this.inputBook.key]);
+    this.router.navigate(["/book-details/", this.inputBook.key]);
   }
-
-
 }
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,20 +1,29 @@
 import { Book } from 'src/app/models/book.model';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { HistoryEntry } from '../models/history.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor(public db: AngularFireDatabase) { }
+  constructor(public db: AngularFireDatabase) { 
+  }
 
   bookList: AngularFireList<any>;
+  bookHistoryList: AngularFireList<any>;
+  historyKeyValue: {[ key: string ]: History} = {};
 
   getBooks(){
     this.bookList = this.db.list('/books');
     return this.bookList.snapshotChanges();
+  }
+
+  getBooksHistory(){
+    this.bookHistoryList = this.db.list('/history');
+    return this.bookHistoryList.valueChanges();
   }
 
   processBooksData(listOfBooks): Book[] {
@@ -27,8 +36,21 @@ export class DashboardService {
     return books;
   }
 
+  processHistoryData(listOfhistory): History[] {
+    const history: History[] = [];
+    listOfhistory.forEach(history => {
+      const newhistory = history.payload.val();
+      newhistory.key = history.key;
+      this.historyKeyValue[history.key] = newhistory;
+    });
+    console.log(this.historyKeyValue)
+    return history;
+  }
+
+
   getBook(key){
     return this.db.object('/books/' + key).valueChanges();
   }
+  
 
 }
