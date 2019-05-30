fdaeb2e6777ddee481e08cc7acf050f60d36026a
denispetriceanu <denispetriceanu@gmail.com>
2019-05-30 10:13:35 +0300

    Version 1 from 30/05/2019

--- src/app/components/admin/manage-books/manage-books.component.ts
+++ src/app/components/admin/manage-books/manage-books.component.ts
@@ -5,6 +5,9 @@ import { Book, BookHistory } from 'src/app/models/book.model';
 import { User } from 'src/app/models/user.model';
 import { LoginService } from 'src/app/services/login.service';
 import { debug } from 'util';
+import { DashboardService } from 'src/app/services/dashboard.service';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-books',
@@ -12,17 +15,29 @@ import { debug } from 'util';
   styleUrls: ['./manage-books.component.css']
 })
 export class ManageBooksComponent implements OnInit {
+  // variabile pentru a prelua date despre carte
+ 
+  // books: Book[];
+  // bookList: AngularFireList<any>;
+  // filtredBooks: Book[];
+  // booksKey: [];
+  //restul de variabile pentru celelalte functii
   booksHistory: HistoryEntry[];
   allBooksByKey: Book[];
   overdueBooks = [];
   borrowedBooks = [];
   allUsersByKey = {};
-  constructor(public bookManagerService: ManageBooksService, public loginService: LoginService) { }
+  history: History[];
+  constructor( 
+    private router: Router,
+    public bookManagerService: ManageBooksService, 
+    public loginService: LoginService,
+    public db: AngularFireDatabase) { }
 
   ngOnInit() {
     this.loginService.usersByKey.subscribe(usersByKey => {
       this.allUsersByKey = usersByKey;
-      console.log(usersByKey);
+      // console.log(usersByKey);
       this.getBooksHistory();
     } );
     this.bookManagerService.booksByKey.subscribe(books => {
@@ -31,7 +46,9 @@ export class ManageBooksComponent implements OnInit {
     this.bookManagerService.history.subscribe(history => {
       this.booksHistory = history;
       this.getBooksHistory();
+      console.log("booksHistory");
     });
+  
   }
 
   getBooksHistory() {
@@ -41,32 +58,35 @@ export class ManageBooksComponent implements OnInit {
     this.overdueBooks = [];
     this.borrowedBooks = [];
 
-    // get books that have no retrun date set - they were not returned
+    // get books that have no return date set - they were not returned
     const borrowedBooks = this.booksHistory.filter(book => !book.returnDate);
     borrowedBooks.forEach( entry => {
       const book: BookHistory = this.allBooksByKey[entry.bookKey];
       book.dueDate = entry.dueDate;
       book.initialDate = entry.initialDate;
       book.returnDate = entry.returnDate;
-      // debugger;
-      if (entry.userKey && this.allUsersByKey[entry.userKey]) {
+      book.key = entry.bookKey;
+      book.userFullName = entry.userKey
+      console.log("FullName" + book.userFullName);
+
+      debugger;
+      if (entry.userKey && this.allUsersByKey[entry.userKey]) { 
         book.userFullName = this.allUsersByKey[entry.userKey].fullName;
       } else {
         book.userFullName = 'User key missing in db';
       }
 
-      const todayDate = this.getTodayDate();
-      console.log(entry.dueDate);
-      if (this.getDateFromString(entry.dueDate) >= todayDate) {
+      let todayDate = this.getTodayDate();
+      // console.log("Due date " + book.dueDate + " " + book.title);
+      // console.log("data de azi " + todayDate);
+      if (this.getDateFromString(book.dueDate) >= todayDate) {
         this.borrowedBooks.push(book);
       } else {
+        // console.log("Cartea este: " + book.dueDate + " " + book.key);
         this.overdueBooks.push(book);
       }
     });
-    console.log('borrowedBooks', this.borrowedBooks);
-    console.log('overdueBooks', this.overdueBooks);
   }
-
 
   getDateFromString(date) {
     const from = date.split('/');
@@ -89,4 +109,9 @@ export class ManageBooksComponent implements OnInit {
     day = (day.length < 2 ? '0' + day : day);
     return `${day}/${month}/${year}`;
   }
+
+  goToDetails(bookKey){
+    this.router.navigate(['/book-details/' + bookKey]);
+  }
+
 }
